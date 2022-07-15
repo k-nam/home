913e4eb4a2725765584f38f444ff6d438433861c
keewoong nam <keewoong.nam@flicspy.com>
2022-07-15 17:26:45 +0900

    Add mfa script

---
+++ shell/mfa.sh
@@ -0,0 +1,12 @@
+set -eu
+
+credentials=$(aws sts get-session-token --token-code $1 --serial-number arn:aws:iam::461691961615:mfa/k-nam --profile k-nam --duration-seconds 129600  \
+--query "Credentials.[AccessKeyId,SecretAccessKey,SessionToken]" --output text)
+
+id=$(echo $credentials | cut -d ' ' -f 1)
+key=$(echo $credentials | cut -d ' ' -f 1)
+token=$(echo $credentials | cut -d ' ' -f 1)
+
+aws configure set profile.mfa.aws_access_key_id "$id"
+aws configure set profile.mfa.aws_secret_access_key "$key"
+aws configure set profile.mfa.aws_session_token "$token"
