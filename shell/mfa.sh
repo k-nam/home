cf5982ca88deef10f1fbf3798e6ebb99230fce4f
keewoong nam <keewoong.nam@flicspy.com>
2022-07-20 09:23:26 +0900

    Refactor

--- shell/mfa.sh
+++ shell/mfa.sh
@@ -1,12 +1,14 @@
 set -eu
 
-credentials=$(aws sts get-session-token --token-code $1 --serial-number arn:aws:iam::461691961615:mfa/k-nam --profile k-nam --duration-seconds 129600  \
---query "Credentials.[AccessKeyId,SecretAccessKey,SessionToken]" --output text)
+OUT=$(aws sts get-session-token --token-code $1 --serial-number arn:aws:iam::461691961615:mfa/k-nam --profile k-nam --duration-seconds 43200)
+export AWS_ACCESS_KEY_ID=$(echo $OUT | jq -r '.Credentials''.AccessKeyId');\
+export AWS_SECRET_ACCESS_KEY=$(echo $OUT | jq -r '.Credentials''.SecretAccessKey');\
+export AWS_SESSION_TOKEN=$(echo $OUT | jq -r '.Credentials''.SessionToken');
 
-id=$(echo $credentials | cut -d ' ' -f 1)
-key=$(echo $credentials | cut -d ' ' -f 2)
-token=$(echo $credentials | cut -d ' ' -f 3)
+# echo $AWS_ACCESS_KEY_ID
+# echo $AWS_SECRET_ACCESS_KEY
+# echo $AWS_SESSION_TOKEN
 
-aws configure set profile.mfa.aws_access_key_id "$id"
-aws configure set profile.mfa.aws_secret_access_key "$key"
-aws configure set profile.mfa.aws_session_token "$token"
+aws configure set profile.mfa.aws_access_key_id "$AWS_ACCESS_KEY_ID"
+aws configure set profile.mfa.aws_secret_access_key "$AWS_SECRET_ACCESS_KEY"
+aws configure set profile.mfa.aws_session_token "$AWS_SESSION_TOKEN"
