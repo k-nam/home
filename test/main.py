7d7909e5f41d81e454c5e97d90071da9549dfb3b
keewoong-nam <keewoong-nam@flicspy.com>
2022-08-12 01:06:12 +0000

    Concurrently create queues

--- test/main.py
+++ test/main.py
@@ -1,6 +1,9 @@
+import threading
+import time
 import time
 import boto3
 from datetime import datetime
+
 def insert_table(id):
     s = boto3.session.Session(region_name='us-east-1', profile_name='test')
     ddb = s.client('dynamodb')
@@ -20,19 +23,35 @@ def create_queue(id):
 
 
 def main():
-    prefix = datetime.now().strftime('%Y%m%d%H%M%S')
+    start = datetime.now()
+    prefix = start.strftime('%Y%m%d%H%M%S')
     print('prefix is', prefix)
-    for n in range(1000*1000):
-        id = prefix + '-' + str(n)
-        if n % 100 == 0:
-            print('now: ',id)
-        try:
-            insert_table(id)
-            create_queue(id)
-        except:
-            time.sleep(60)
+    len = 10
+    concurrency = 1
 
+    def run(thread_id):
+        for n in range(len):
+            id = prefix + '-' + str(n) + '-' + thread_id
+            if n % 100 == 0:
+                print('now: ',id)
+            try:
+                insert_table(id)
+                create_queue(id)
+            except:
+                time.sleep(60)
 
+    threads = []
+    for i in range(concurrency):
+        t = threading.Thread(target=run, args=(f'thread-{i}',))
+        threads.append(t)
+        t.start()
+
+    for i in range(concurrency):
+        threads[i].join()
+
+    end = datetime.now()
+
+    print('Throughput: ' + str(len*concurrency / (end - start).total_seconds()) + ' items/sec')
 
 if __name__ == '__main__':
     main()
