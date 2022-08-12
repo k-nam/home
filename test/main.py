4d37f56cd068ea53b1cc9989d02d513c53acbc54
keewoong-nam <keewoong-nam@flicspy.com>
2022-08-12 02:43:06 +0000

    Send messages

--- test/main.py
+++ test/main.py
@@ -1,13 +1,16 @@
+from concurrent.futures import thread
 import threading
 import time
 import time
 import boto3
 from datetime import datetime
 
+session = boto3.session.Session(region_name='us-east-1', profile_name='test')
+ddbClient = session.client('dynamodb')
+sqsClient = session.client('sqs')
+
 def insert_table(id):
-    s = boto3.session.Session(region_name='us-east-1', profile_name='test')
-    ddb = s.client('dynamodb')
-    ddb.put_item(
+    ddbClient.put_item(
         TableName='queue-record',
         Item={
             'user_id': {
@@ -17,17 +20,22 @@ def insert_table(id):
     )
 
 def create_queue(id):
-    s = boto3.session.Session(region_name='us-east-1', profile_name='test')
-    q = s.client('sqs')
-    q.create_queue(QueueName=id,)
+    sqsClient.create_queue(QueueName=id,)
 
+def send_messages(start, range):
+    queue_prefix = '20220805090227'
+    index = start
+    while index < start + range:
+        sqsClient.send_message(QueueUrl=f'https://sqs.us-east-1.amazonaws.com/721756646069/{queue_prefix}-{index}', MessageBody='sample message')
 
-def main():
+        index += 1
+
+def create_many_queues():
     start = datetime.now()
     prefix = start.strftime('%Y%m%d%H%M%S')
     print('prefix is', prefix)
-    len = 10
-    concurrency = 1
+    len = 50
+    concurrency = 30
 
     def run(thread_id):
         for n in range(len):
@@ -53,5 +61,31 @@ def main():
 
     print('Throughput: ' + str(len*concurrency / (end - start).total_seconds()) + ' items/sec')
 
+def send_many_msgs():
+    start_time = datetime.now()
+    range = 1000
+    end = 100000
+    duplicate = 2
+    start = 0
+    threads = []
+    while start + range <= end:
+        i = 0
+        while i < duplicate:
+            t = threading.Thread(target=send_messages,args=(start,range,))
+            threads.append(t)
+            t.start()
+            i += 1
+        start += range
+
+    i = 0
+    for t in threads:
+        t.join()
+
+
+    end_time = datetime.now()
+
+    print('send message Throughput: ' + str(end*duplicate / (end_time - start_time).total_seconds()) + ' items/sec')
+
 if __name__ == '__main__':
-    main()
+    # create_many_queues()
+    send_many_msgs()
