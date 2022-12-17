eb345283653f36ace3c379ce3916fd5de1e41cc6
keewoong nam <keewoong.nam@flicspy.com>
2022-12-17 09:22:04 +0900

    Improve

--- shell/gpsl.sh
+++ shell/gpsl.sh
@@ -1,6 +1,12 @@
 set -eux
 
+CUR_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+if [ $CUR_BRANCH == "master" ]; then
+    echo "Currently on master, abort"
+    exit 1
+fi
+
 git checkout master
 git pull
 git fetch --prune
-git branch -D "$1"
+git branch -D $CUR_BRANCH
