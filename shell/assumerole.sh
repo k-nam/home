40805f554da4e1dd410ab11187da6b48ec442bb3
KEEWOONG NAM <keewoong.nam@flicspy.com>
2023-06-28 18:18:09 +0900

    Add assume role

---
+++ shell/assumerole.sh
@@ -0,0 +1,8 @@
+
+AWS_ACCESS_KEY_ID=
+AWS_PROFILE=k-nam
+OUT=$(aws sts assume-role --role-arn $1 --role-session-name script-session --duration-seconds 3600)
+export AWS_ACCESS_KEY_ID=$(echo $OUT | jq -r '.Credentials''.AccessKeyId');\
+export AWS_SECRET_ACCESS_KEY=$(echo $OUT | jq -r '.Credentials''.SecretAccessKey');\
+export AWS_SESSION_TOKEN=$(echo $OUT | jq -r '.Credentials''.SessionToken');
+
