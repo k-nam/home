e18aff312e871f7db7baf0412390481855b21068
KEEWOONG NAM <keewoong.nam@flicspy.com>
2023-10-15 11:23:21 +0900

    Fix

--- shell/assumerole.sh
+++ shell/assumerole.sh
@@ -1,7 +1,9 @@
+set -u
 
-AWS_ACCESS_KEY_ID=
-AWS_PROFILE=k-nam
-OUT=$(aws sts assume-role --role-arn $1 --role-session-name script-session --duration-seconds 3600)
+PROFILE=$1
+ROLE_ARN=$2
+OUT=$(aws sts assume-role --role-arn $ROLE_ARN --role-session-name script-session --duration-seconds 900 --profile $PROFILE)
+echo $OUT
 export AWS_ACCESS_KEY_ID=$(echo $OUT | jq -r '.Credentials''.AccessKeyId');\
 export AWS_SECRET_ACCESS_KEY=$(echo $OUT | jq -r '.Credentials''.SecretAccessKey');\
 export AWS_SESSION_TOKEN=$(echo $OUT | jq -r '.Credentials''.SessionToken');
