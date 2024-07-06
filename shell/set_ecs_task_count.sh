68e1e4af4c86fb33571b213d57a90f3ff9f7c9c8
KEEWOONG NAM <keewoong.nam@flicspy.com>
2024-07-06 16:35:24 +0900

    Stop using self hosted runner

--- shell/set_ecs_task_count.sh
+++ shell/set_ecs_task_count.sh
@@ -2,7 +2,7 @@ set -eux
 
 COUNT=$1
 
-aws ecs update-service --cluster f-admin-github-actions-runner --service flicspy-c7g-medium --desired-count $COUNT --region ap-south-2 --profile f-admin > /dev/null &
+# aws ecs update-service --cluster f-admin-github-actions-runner --service flicspy-c7g-medium --desired-count $COUNT --region ap-south-2 --profile f-admin > /dev/null &
 
 aws ecs update-service --cluster flicspy-stg-1-ms-activity --service flicspy-stg-1-ms-activity-main --desired-count $COUNT --region ap-northeast-2 --profile f-dev > /dev/null &
 aws ecs update-service --cluster flicspy-stg-1-ms-content --service flicspy-stg-1-ms-content-main --desired-count $COUNT --region ap-northeast-2 --profile f-dev > /dev/null &
